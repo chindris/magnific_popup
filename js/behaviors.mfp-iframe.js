a3d65c2bc34e3d44a7e0a65f36c182100e2b224a
Jay Dansand <dansandj@799552.no-reply.drupal.org>
2013-10-14 07:20:14 -0500

    Add Gallery Type options to formatter.

--- js/behaviors.mfp-iframe.js
+++ js/behaviors.mfp-iframe.js
@@ -1,15 +1,17 @@
 (function ($) {
   Drupal.behaviors.magnific_popup_iframe = {
 		attach: function (context, settings) {
-      $('.magnific-popup-iframe-gallery').once('magnific_popup_iframe').magnificPopup({
-        delegate: 'a',
-        gallery: { enabled: true },
+      var common_options = {
         disableOn: 300,
-        type: 'iframe',
         mainClass: 'mfp-fade',
         removalDelay: 160,
         preloader: false,
-        fixedContentPos: 'auto',
+        fixedContentPos: 'auto'
+      };
+      var gallery_options = {
+        gallery: {enabled: true},
+        type: 'iframe',
+        delegate: 'a',
         iframe: {
           markup: '<div class="mfp-iframe-scaler"><div class="mfp-close"></div><iframe class="mfp-iframe" id="mfp-iframe" frameborder="0" seamless="seamless" allowfullscreen="allowfullscreen"></iframe></div>',
           patterns: {
@@ -20,6 +22,15 @@
             }
           }
         }
+      };
+      var singleton_options = {
+        type: 'image',
+      };
+      $.extend(singleton_options, common_options);
+      $('.magnific-popup-no-gallery-item').once('magnific_popup').magnificPopup(singleton_options);
+      $.extend(gallery_options, common_options);
+      $('.magnific-popup-iframe-gallery').once('magnific_popup').each(function(el, i) {
+        $(this).magnificPopup(gallery_options);
       });
     }
   };
